81add84be6d206738a380c361c61629323bb444b
kapssome <84270468+kapssome@users.noreply.github.com>
2024-10-13 23:54:15 +0530

    Update ques.js

--- js/ques.js
+++ js/ques.js
@@ -2,60 +2,218 @@
 let questions = [
     {
     numb: 1,
-    question: "?",
-    answer: "Hyper Text Markup Language",
+    question: "Which city was the host to the 2024 Summer Olympics?",
+    answer: "Paris",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+      "London",
+      "Tokyo",
+      "Paris",
+      "New York"
     ]
   },
     {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question: "What is the purest form of gold?",
+    answer: "24 karat",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
+      "91.6 karat",
+      "18 karat",
+      "22 karat",
+      "24 karat"
     ]
   },
     {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
+    question: "Which country's currency code is TRY?",
+    answer: "Turkey",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+      "Trinidad and Tobago",
+      "Turkmenistan",
+      "Turkey",
+      "Tanzania"
     ]
   },
     {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
+    question: "Which metal exists as liquid at room temperature?",
+    answer: "Mercury",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
+      "Lead",
+      "Mercury",
+      "Gold",
+      "Zinc"
     ]
   },
     {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    question: "Who developed C programming language?",
+    answer: "Dennis Ritchie",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "Ken Thompson",
+      "Rob Pike",
+      "Dennis Ritchie",
+      "Bjarne Stroustrup"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
+    {
+    numb: 6,
+    question: "What is the escape velocity from the earth's surface?",
+    answer: "11.2 km/s",
+    options: [
+      "11.2 km/s",
+      "9.8 m/s",
+      "11.2 m/s",
+      "9.8 km/s"
+    ]
+  },
+    {
+    numb: 7,
+    question: "The short-sightedness of the eye is called?",
+    answer: "Myopia",
+    options: [
+      "Amblyopia",
+      "Glaucoma",
+      "Myopia",
+      "Cataract"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Who was the second president of America?",
+    answer: "John Adams",
+    options: [
+      "George Washington",
+      "Abraham Lincoln",
+      "Thomas Jefferson",
+      "John Adams"
+    ]
+  },
+    {
+    numb: 9,
+    question: "What kind of books were recognized by Nebula award?",
+    answer: "Science Fiction",
+    options: [
+      "Comedy",
+      "Science Fiction",
+      "Romance",
+      "Horror"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Who invented dynamite?",
+    answer: "Alfred Nobel",
+    options: [
+      "Alfred Nobel",
+      "Immanuel Nobel",
+      "Marie Curie",
+      "Albert Einstein"
+    ]
+  },
+    {
+    numb: 11,
+    question: "What is the chemical formula for Methane?",
+    answer: "CH4",
+    options: [
+      "CH4",
+      "C2H6",
+      "C3H8",
+      "C2H2"
+    ]
+  },
+    {
+    numb: 12,
+    question: "Which one of the below sequences is arithmetic progression?",
+    answer: "2, 5, 8, 11, 14...",
+    options: [
+      "2, 5, 8, 11, 15...",
+      "2, 5, 8, 12, 14...",
+      "2, 5, 8, 11, 14...",
+      "2, 4, 8, 11, 14..."
+    ]
+  },
+    {
+    numb: 13,
+    question: "Which country won the first Football World Cup?",
+    answer: "Uruguay",
+    options: [
+      "Argentina",
+      "Uruguay",
+      "Paraguay",
+      "Brazil"
+    ]
+  },
+    numb: 14,
+    question: "Which is the boundary line between India and Pakistan?",
+    answer: "Radcliffe",
+    options: [
+      "Radcliffe",
+      "McMohan",
+      "Durand",
+      "Siegfried"
+    ]
+  },
+    numb: 15,
+    question: "Which of the following disease affects the eyes?",
+    answer: "Glaucoma",
+    options: [
+      "Otosclerosis",
+      "Diarrhoea",
+      "Tonsillitis",
+      "Glaucoma"
+    ]
+  },
+    numb: 16,
+    question: "Which one of the following is located between the orbits of the planets Jupiter and Mars?",
+    answer: "Asteroid belt",
+    options: [
+      "Asteroid belt",
+      "Kuiper belt",
+      "Orion's belt",
+      "Debris disk"
+    ]
+  },
+    numb: 17,
+    question: "Name the world's first artificial nuclear reactor?",
+    answer: "Chicago Pile-1",
+    options: [
+      "Experimental Breeder",
+      "X-10 Graphite",
+      "Chicago Pile-1",
+      "Apsara"
+    ]
+  },
+    numb: 18,
+    question: "The Oval stadium in London is associated with?",
+    answer: "Cricket",
+    options: [
+      "Football",
+      "Tennis",
+      "Rugby",
+      "Cricket"
+    ]
+  },
+    numb: 19,
+    question: "Which country's official language is Sinhalese?",
+    answer: "Sri Lanka",
+    options: [
+      "Malaysia",
+      "Sri Lanka",
+      "Singapore",
+      "Thailand"
+    ]
+  },
+    numb: 20,
+    question: "What is the size of IPv6 address?",
+    answer: "128 bits",
+    options: [
+      "16 bits",
+      "32 bits",
+      "64 bits",
+      "128 bits"
+    ]
+  },
+   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
   //   {
   //   numb: 6,
